5fc8e41355cb19bb543c99e6b0c676a6fd8bf214
Krishna <krisnachhetri07@gmail.com>
2023-04-11 22:57:27 +1000

    routing

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ const HomePage = () => {
       <h1 className="text-3xl font-bold mb-4">Welcome to Linkify</h1>
       <div className="flex justify-between">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Sign up
+          <Link href={"/user_auth/signup"}> Sign up</Link>
         </button>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Sign in
+          <Link href={"/user_auth/signin"}> Sign up</Link>
         </button>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           <Link href="/demo"> Demo</Link>
