8f2e55e3e8f60cae865320581f362b7ecc5648f0
Krishna <krisnachhetri07@gmail.com>
2023-06-06 21:52:35 +1000

    button hover effect

--- linkify/src/app/Landingpage/Components/ui/Button.tsx
+++ linkify/src/app/Landingpage/Components/ui/Button.tsx
@@ -4,11 +4,12 @@ import { cva, VariantProps } from "class-variance-authority";
 
 import { cn } from ".././../../../lib/utils";
 import { FaArrowRight, FaChevronRight } from "react-icons/fa";
+
 const buttonVarients = cva("active:", {
   variants: {
     variant: {
       default:
-        "flex  items-center gap-2 bg-slate-900 text-white hover:bg-red-800 px-4 py-2",
+        "flex relative  items-center gap-2 bg-slate-900 text-white  px-4 py-2 button ",
       ghost: "bg-transparent hover:text-slate-900 hover:bg-slate-200",
       primary: "bg-gray-500 gap-2 px-4 py-2 rounded-full text-white",
     },
@@ -53,8 +54,11 @@ const Button = ({
       onMouseLeave={() => setHover(!hover)}
     >
       {/* {isLoading ? <Loader2 className="mr-2 h-4 w-5 animate-spin" /> : null} */}
-      {children}
-      {icon ? !hover ? <FaArrowRight /> : <FaChevronRight /> : ""}
+      <div className="flex items-center justify-center gap-2 relative z-[99]">
+        {" "}
+        {children}
+        {icon ? !hover ? <FaArrowRight /> : <FaChevronRight /> : ""}
+      </div>
     </button>
   );
 };
