cf7ccdb6e609ba7ee31644836a9dcb0218949ba8
Krishna <krisnachhetri07@gmail.com>
2023-06-13 23:56:54 +1000

    template templates

--- linkify/src/app/templates/page.tsx
+++ linkify/src/app/templates/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="templatesanimationshere ">
+      {/* hero section with templates animation */}
+      {/* {show some templates } */}
+      {/* Mthod of creation */}
+      {/* free to use options */}
+      {/* social media options */}
+      {/* design colors  */}
+    </div>
+  );
 };
 
 export default page;
