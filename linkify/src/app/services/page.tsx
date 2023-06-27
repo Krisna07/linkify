380415a84ffead25f8f7a40150179f8ea3bf50d8
Krishna <krisnachhetri07@gmail.com>
2023-06-27 21:58:53 +1000

    service initiative

--- linkify/src/app/services/page.tsx
+++ linkify/src/app/services/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="w-full grid">
+      <div className="w-full bg-gradient-to-r from-slate-800 to-slayte-600 py-40 grid place-items-center"></div>
+    </div>
+  );
 };
 
 export default page;
