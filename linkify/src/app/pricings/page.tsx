fda1fc54d32adff251120eaa3a55df31c74f8dec
Krishna <krisnachhetri07@gmail.com>
2023-06-29 23:32:21 +1000

    pricing page initiated

--- linkify/src/app/pricings/page.tsx
+++ linkify/src/app/pricings/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Templates = () => {
-  return <div>Templates</div>;
+const Pricing = () => {
+  return (
+    <div className="w-full p-8 grid place-items-center">
+      <div className="w-full bg-gray-300 grid place-items-center">
+        <div className="lg:w-[1000px] grid place-items-center">
+          <h2 className="font-bold text-[32px]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+            aut?
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+            neque! Aperiam ullam pariatur iure reprehenderit similique officiis
+            inventore sequi dolores.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Templates;
+export default Pricing;
