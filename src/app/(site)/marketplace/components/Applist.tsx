f9ddb23c1fdd50b1c23ee531abf240f342770255
Krishna Paudel <krisnachhetri07@gmail.com>
2023-09-17 22:21:06 +1000

    des

--- src/app/(site)/marketplace/components/Applist.tsx
+++ src/app/(site)/marketplace/components/Applist.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
 import { IconType } from "react-icons/lib";
 
 interface Applist {
@@ -9,29 +10,66 @@ interface Applist {
   accent: string;
   type: string;
 }
-const Applist = ({ applications }: any) => {
+
+const ApplistComponent = ({ applications }: any) => {
   const [showapp, setShowapp] = useState<boolean>(false);
   const title = applications[0].type;
+
+  let description = "";
+  switch (title) {
+    case "productive":
+      description =
+        "Share the app that shows how productive you are. Share your creative projects.";
+      break;
+    case "entertainment":
+      description =
+        "Share your favorite shows and songs. Let your connections know what you are hyping at.";
+      break;
+    case "shopping":
+      description =
+        "Expand your marketing. Share the websites and e-commerce websites with people.";
+      break;
+    case "social media":
+      description =
+        "Grow your connections. Make your connections, friends, and followers easy to connect to you.";
+      break;
+    default:
+      description = "Description not available";
+      break;
+  }
 
   return (
     <div className="w-full grid gap-8 box-border">
       <div className="flex w-full justify-between items-center">
         <h2 className="text-2xl font-bold">{title.toUpperCase()}</h2>
         <button
-          className=" font-semibold text-slate-800 cursor-pointer hover:text-sky-600 "
+          className="font-semibold text-slate-800 cursor-pointer hover:text-sky-600"
           onClick={() => setShowapp(!showapp)}>
           {showapp ? " less" : `+see more ${applications.length - 6}`} apps
         </button>
       </div>
-      <div className="grid gap-4">
-        <div className="flex items-center gap-4">
-          <div
-            className={`w-fit bg-${applications[0].accent} text-[80px] p-4  shadow-bs`}>
-            {applications[0].logo}
+      <div className="grid gap-6">
+        <div className="grid tablet:grid-cols-2 ">
+          <div className="flex items-center gap-4">
+            <div
+              className={`w-fit bg-${applications[0].accent} text-[80px] p-4  shadow-bs`}>
+              {applications[0].logo}
+            </div>
+            <div>
+              <h2 className="text-xl font-[600]">{applications[0].name}</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quibusdam, modi!
+              </p>
+            </div>
           </div>
-          <div>
-            <h2 className="text-xl font-[600]">{applications[0].name}</h2>
-            <p>{applications[0].des}</p>
+          <div className="p-4  rounded-lg shadow-bs grid place-items-center text-md relative">
+            <div className="absolute w-8 h-8 bg-yellow-400 rounded-full top-1 right-1 grid place-items-center">
+              <div className="w-8 h-8 right-1 top-0 bg-black relative rounded-full grid hover:-rotate-45 text-white hover:text-yellow-300 place-items-center transition-all">
+                <FaArrowRight />
+              </div>
+            </div>
+            {description}
           </div>
         </div>
         <div className="w-full grid md:grid-cols-2 laptop:grid-cols-3 tablet:grid-cols-2 gap-4 ">
@@ -57,4 +95,4 @@ const Applist = ({ applications }: any) => {
   );
 };
 
-export default Applist;
+export default ApplistComponent;
