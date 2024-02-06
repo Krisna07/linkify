5aab39a6739b27ff70ab5a363223f4bc5ddd0489
Krishna Paudel <krisnachhetri07@gmail.com>
2024-02-06 06:46:06 +0000

    dynamic password field

--- src/app/(auth)/auth/Formcomponents/Input.tsx
+++ src/app/(auth)/auth/Formcomponents/Input.tsx
@@ -1,41 +1,57 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, ChangeEvent } from "react";
 
-interface input {
-  lable?: string;
-  icon?: any;
+interface InputProps {
+  label?: string;
+  icon?: React.ReactNode;
   color?: string;
   placeholder?: string;
-  data?: string;
+  type?: string;
+  secondIcon?: any;
 }
 
-const Input = ({ lable, icon, color, placeholder, data }: input) => {
+const Input: React.FC<InputProps> = ({
+  label,
+  icon,
+  color = "gray",
+  placeholder,
+  type,
+  secondIcon,
+}) => {
   const [value, setValue] = useState<string>("");
   const [active, setActive] = useState<boolean>(false);
+  const [inputtype, setType] = useState<boolean>(false);
 
-  const onchange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-    console.log(value);
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+    console.log(newValue);
   };
+
   return (
     <label className={`w-fit p-2 bg-${color} grid gap-2`}>
-      <span className="text-[16px] font-semibold">{lable}</span>
+      <span className="text-[16px] font-semibold">{label}</span>
 
       <div className="relative">
         <input
-          type="text"
+          type={inputtype ? "text" : type}
           onFocus={() => setActive(true)}
           onBlur={() => setActive(false)}
           placeholder={placeholder}
-          onChange={onchange}
+          value={value}
+          onChange={onChange}
           className="outline-none shadow-[0_0_4px_0_gray] px-2 py-1 rounded"
         />
-        <div
-          className={`absolute right-[2px] top-[2px]  rounded-full text-[12px] p-2 ${
-            active ? "shadow bg-green-300 " : "shadow-none bg-gray-300"
-          }`}
-        >
-          {icon}
+
+        <div className="absolute right-0 top-0 h-full bg-white px-2 grid place-items-center">
+          <div
+            className={` right-[2px] top-[2px] rounded-full text-[12px] p-2 ${
+              active ? "shadow bg-green-300" : "shadow-none bg-gray-300"
+            }`}
+            onClick={() => setType(!inputtype)}
+          >
+            {inputtype ? secondIcon : icon}
+          </div>
         </div>
       </div>
     </label>
--- src/app/(auth)/auth/signin/page.tsx
+++ src/app/(auth)/auth/signin/page.tsx
@@ -1,21 +1,53 @@
-import React from "react";
-import { BsEyeSlash } from "react-icons/bs";
+"use client";
+import React, { useState } from "react";
+import { BsEye, BsEyeSlash } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
 import Input from "../Formcomponents/Input";
 import Button from "@/app/g_components/Button";
 
-const page = () => {
+interface User {
+  username: string;
+  password: string;
+}
+
+const SignInPage: React.FC = () => {
+  const [formData, setFormData] = useState<User>({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log("Form submitted:", formData);
+  };
+
   return (
     <div className="w-fit p-2 px-4 bg-gradient-to-tr from-indigo-400 to-blue-300 grid place-items-center">
       <div className="w-full px-2 text-lg font-bold">
-        {" "}
-        <h3>Sigin</h3>
+        <h3>Sign in</h3>
       </div>
-      <Input lable="Username" placeholder="Username" icon={<FaUser />} />
-      <Input lable="Password" placeholder="Password" icon={<BsEyeSlash />} />
-      <Button children="Submit" variant={"default"} size={"default"} />
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="Username"
+          placeholder="Username"
+          icon={<FaUser />}
+          color="blue"
+        />
+        <Input
+          label="Password"
+          type="password"
+          placeholder="Password"
+          icon={<BsEyeSlash />}
+          secondIcon={<BsEye />}
+          color="green"
+        />
+        <Button type="submit" variant={"default"} size={"default"}>
+          Submit
+        </Button>
+      </form>
     </div>
   );
 };
 
-export default page;
+export default SignInPage;
