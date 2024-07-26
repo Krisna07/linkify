ab16d515d1a86f815019c764f0d1e99759fbd1ef
krisnachhetri07 <krisnachhetri07@gmail.com>
2024-07-26 07:42:34 +0000

    background landing page

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,28 +1,30 @@
+
 import "../globals.css";
 
-import Landingpage from "../../components/Layouts/Landingpage";
+// import Landingpage from "../../components/Layouts/Landingpage";
 import { Inter } from "next/font/google";
 import Button from "../../components/Global_components/Button";
 import { FaLeaf } from "react-icons/fa";
 import Link from "next/link";
-
+import Background from "../../components/Landing_components/Background";
 const inter = Inter({ subsets: ["latin"] });
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body
-        className={`${inter.className} w-full h-fit p-0 m-0 grid place-items-center  dark:bg-dark leading -[100%]  `}
+        className={`${inter.className} w-full h-fit p-0 m-0 grid place-items-center  bg-dark leading -[100%]  text-silver relative`}
       >
-        <div className="w-full h-screen grid place-items-center ">
-          <div className="grid place-items-center gap-4 p-2 shadow-bs rounded-lg text-center">
+        <Background/>
+        <div className="w-full h-screen grid place-items-center  relative z-40 ">
+          <div className="grid place-items-center gap-4 p-2 shadow-bs rounded-lg text-center bg-primary p-4">
             <span className="flex gap-2 items-center text-xl font-bold">
               Linkify <FaLeaf color="green" />
             </span>
@@ -39,7 +41,7 @@ export default function RootLayout({
               />
             </Link>
             <Button
-              variant={"primary"}
+              variant={"accent"}
               children="Sign up"
               size={"default"}
               icon={true}
